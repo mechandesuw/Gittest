8d88e8a4ba3ca74d389c18bc2c187c0b4c9b0293
mechandesuw <w.mick_ovo@ezweb.ne.jp>
2019-04-07 15:21:36 +0900

    Initial Commit

---
+++ gittest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by 山谷美咲生 on 2019/04/07.
+//  Copyright © 2019 山谷美咲生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
